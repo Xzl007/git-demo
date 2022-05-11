c21ae10e4c395ee67b0cbed7eaad497d447924c4
summer <780606289@qq.com>
2022-05-11 15:00:24 +0800

    master second commit

--- src/com/GitTest/gitDemo.java
+++ src/com/GitTest/gitDemo.java
@@ -3,5 +3,6 @@ package com.GitTest;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("test1!");
+        System.out.println("second test");
     }
 }
