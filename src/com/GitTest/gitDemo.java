229ac7463ed59a6b13cc9724c209fadf5fcfb0c2
summer <780606289@qq.com>
2022-05-11 15:23:44 +0800

    hot-fix first commit

--- src/com/GitTest/gitDemo.java
+++ src/com/GitTest/gitDemo.java
@@ -4,5 +4,7 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("test1!");
         System.out.println("second test");
+        System.out.println("hot-fix branch test");
+        System.out.println("hot-fix branch test2");
     }
 }
